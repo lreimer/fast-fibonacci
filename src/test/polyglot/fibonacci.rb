904337fdb90eb74a7c0500bbd20e3beb7c71f2bf
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2021-01-29 08:33:28 +0100

    Added Fibonacci in Python and Ruby.

---
+++ src/test/polyglot/fibonacci.rb
@@ -0,0 +1,15 @@
+puts 'Fibonacci in Ruby on GraalVM!'
+
+n = 42
+
+def fibonacci(n)
+    if n == 1
+        1
+    elsif n == 2
+        1
+    else
+        fibonacci(n-1) + fibonacci(n-2)
+    end
+end
+
+puts "#{n}'s fibonacci value is #{fibonacci(n)}"
